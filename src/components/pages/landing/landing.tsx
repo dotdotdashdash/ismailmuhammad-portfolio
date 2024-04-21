279a8e988f6afab1ac81fea087ef8776015b3568
Ismail <ismailir@alokin.in>
2024-04-21 12:13:59 +0530

    add support for gh-pages deployment

--- src/components/pages/landing/landing.tsx
+++ src/components/pages/landing/landing.tsx
@@ -6,11 +6,11 @@ const LandingPage: React.FC = () => {
   return (
 
     <div className="landing-page d-flex justify-content-center align-items-center vh-100">
-    <div className="col-lg-4 paragraph-container text-left">
+    <div className="col-xl-4 col-lg-5 col-md-6 col-sm-8 col-12 paragraph-container text-left">
       <p className="centered-paragraph ">
       👋 <br/>Hey, Ismail here! <br/><br/>
       I'm a passionate Software Engineer, focused primarily on building robust and scalable web backend solutions.<br/><br/>
-      I possess expertise in leveraging Go(Golang) and Node.JS to build systems that outlive the test of time and space.<br/><br/>
+      I possess proficiency in leveraging Go(Golang) and Node.JS to build systems that outlive the test of time and space.<br/><br/>
       You can get in touch with me at <strong>hello@ismailmuhammad.com</strong>.
       </p>
     </div>
